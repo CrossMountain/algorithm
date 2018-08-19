ee5c80139ca1041f18a2eecbcd74e732822050bb
crossmountain <954928769@qq.com>
2018-08-19 10:04:37 +0800

    新增对赛码网的支持

--- js/stdio.js
+++ js/stdio.js
@@ -2,9 +2,13 @@ var _input = `
 `
 
 var _reg_ = /.+/gm
-
+//nowcoder
 function readline() {
     return _reg_.exec(_input)[0]
 }
 
-
+//acmcoder
+function read_line() {
+    return _reg_.exec(_input)[0]
+}
+var print = console.log.bind(console)
